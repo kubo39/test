b6ea113fdbc484e8a73e3579dfc65013d1f8d958
Hiroki Noda <kubo39@gmail.com>
2016-12-02 00:25:04 +0900

    Cleanup

--- rust/asm/test.asm
+++ rust/asm/test.asm
@@ -4,11 +4,7 @@ global plus_one
 
 section .text
 
-%macro epilog 0
-        ret
-%endmacro
-
 plus_one:
-        add rdi, 1
-        mov rax, rdi
-        epilog
+    add rdi, 1
+    mov rax, rdi
+    ret
