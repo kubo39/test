2855fd07899836bbc2e52696535aa236b7afb3a9
Hiroki Noda <kubo39@gmail.com>
2016-12-23 01:58:10 +0900

    Add rayon sample.

---
+++ rust/parallelism/src/main.rs
@@ -0,0 +1,21 @@
+extern crate rayon;
+#[macro_use]
+extern crate approx;
+
+use rayon::prelude::*;
+
+static N: usize = 1_000_000 - 1;
+static DELTA: f64 = 1.0f64 / 1_000_000.0f64;
+
+fn get_term(i: usize) -> f64 {
+    let x = (i as f64 - 0.5f64) * DELTA;
+    DELTA / (1.0 + x * x)
+}
+
+fn main() {
+    let pi: f64 = 4.0 * (0..N).collect::<Vec<_>>()
+        .par_iter()
+        .map(|&i| get_term(i) )
+        .sum();
+    assert_relative_eq!(pi, 3.1415926f64, max_relative = 0.001f64);
+}
