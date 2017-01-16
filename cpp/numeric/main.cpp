888d996065c0f670bde474d0cd079fbf8a6abb5b
Hiroki Noda <kubo39@gmail.com>
2017-01-16 18:05:05 +0900

    accumulate追加

--- cpp/numeric/main.cpp
+++ cpp/numeric/main.cpp
@@ -5,10 +5,13 @@
 int main(void)
 {
   std::vector<int32_t> vec(10);
-  std::iota(vec.begin(), vec.end(), 1);
+  std::iota(vec.begin(), vec.end(), 1);  // [1,2,3,4,5,6,7,8,9,10]
   for (auto i : vec) {
     std::cout << i << " ";
   }
   std::cout << std::endl;
+
+  auto sum = std::accumulate(vec.begin(), vec.end(), 0);
+  std::cout << sum << std::endl;
   return 0;
 }
