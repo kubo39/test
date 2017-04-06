8e3b3f439ae845bddc125431a6b91aacf56bc625
Hiroki Noda <kubo39@gmail.com>
2017-04-06 17:32:46 +0900

    simpleにした

--- rust/rust-fp-test/main.rs
+++ rust/rust-fp-test/main.rs
@@ -3,23 +3,6 @@ struct MyStruct {
     init: unsafe extern "C" fn(*mut MyStruct),
 }
 
-struct Outer {
-    inner: *mut MyStruct,
-}
-
-impl Outer {
-    fn new() -> Self {
-        Outer {
-            inner: unsafe { getInstance() },
-        }
-    }
-
-    fn init(&mut self) {
-        let raw = unsafe { &*self.inner };
-        unsafe { (raw.init)(self.inner) };
-    }
-}
-
 #[link(name="aaa")]
 extern "C" {
     fn getInstance() -> *mut MyStruct;
@@ -27,6 +10,6 @@ extern "C" {
 
 fn main() {
     // 静的に生成されるstructへのポインタを構造体として扱う
-    let mut outer = Outer::new();
-    outer.init();
+    let mut ptr = unsafe { getInstance() };
+    unsafe { ((&*ptr).init)(ptr) };
 }
