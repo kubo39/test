5fee415de5ca7bc4a6f13ae2262be1b1d4e612f5
Hiroki Noda <kubo39@gmail.com>
2019-02-16 02:45:23 +0900

    F#: introduction

---
+++ fsharp/introduction/Program.fs
@@ -0,0 +1,96 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+// 1.1.1 a lightweight syntax
+let a_lightweight_syntax _ =
+    let numbers = [ 1 .. 10 ]
+    let square x = x * x in
+    let squares = numbers |> List.map square
+    printfn "N^2 = %A" squares
+
+    let computeDerivative f x =
+        let p1 = f (x - 0.05)
+        let p2 = f (x + 0.05)
+        (p2 - p1) / 0.1
+
+    // おまけ
+    let sum = squares |> List.fold (fun acc x -> x + acc) 0
+    printfn "Sum = %A" sum
+
+// 1.1.2 making data simple
+let making_data_simple _ =
+    let vowels = ['e'; 'i'; 'o'; 'u']
+    printfn "%A" vowels
+    printfn "%A" (['a'] @ vowels) // 結合
+
+    let tuple = (1, false, "text")
+    let getNumberInfo (x: int) = (x, x.ToString(), x * x) in
+    printfn "%A" (getNumberInfo 42)
+
+// 1.1.3 making types simple
+let making_types_simple _ =
+    let swap (a, b) = (b, a) in
+    swap(1, 2) |> ignore
+    swap("you", true) |> ignore
+
+// 1.1.4 functional programming
+let functional_programming _ =
+    let arr = [ 1 .. 5] |> List.map (fun x -> x % 2 = 0)
+    printfn "%A" arr
+
+    let checkList alist =
+        match alist with
+        | [] -> 0
+        | [a] -> 1
+        | [a; b] -> 2
+        | [a; b; c] -> 3
+        | _ -> failwith "List is too big!"
+        in
+
+    // let getType (x : obj) =
+    //     match x with
+    //     | :? string -> "x is string"
+    //     | :? int -> "x is int"
+    //     | :? System.Exception -> "x is an exception"
+    //     in
+
+    let square x = x * x in
+    let toStr (x : int) = x.ToString() in
+    let reverse (x : string) = new System.String(Array.rev (x.ToCharArray())) in
+    let result = 32 |> square |> toStr |> reverse
+    printfn "%A" result
+
+// 1.1.5 imperative programming
+let imperative_programming _ =
+    printfn "%d * %f = %s" 5 0.75 ((5.0 * 0.75).ToString())
+
+// 1.1.7 parallel and asynchronous programming
+let parallel_and_asynchronous_programming _ =
+    let rec fib x = if x < 2 then 1 else fib(x-1) + fib(x-2) in
+    let fibs =
+        Async.Parallel [ for i in 0..40 -> async { return fib(i) } ]
+        |> Async.RunSynchronously
+    printfn "The fibonacci numbers are %A" fibs
+
+[<EntryPoint>]
+let main argv =
+    // 1.1.1 lightweight syntax
+    a_lightweight_syntax ()
+
+    // 1.1.2 making data simple
+    making_data_simple ()
+
+    // 1.1.3 making types simple
+    making_types_simple ()
+
+    // 1.1.4 functional programming
+    functional_programming ()
+
+    // 1.1.5 imperative programming
+    imperative_programming ()
+
+    // 1.1.7 parallel and asynchronous programming
+    parallel_and_asynchronous_programming ()
+
+    0 // return an integer exit code
